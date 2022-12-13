88714093444f11858e4d3e1ab7d04b4d42724a77
Thomas Whiteway <thomas.whiteway@gmail.com>
2022-12-13 08:43:10 +0000

    Day 13 Part 2

--- src/day13.rs
+++ src/day13.rs
@@ -1,5 +1,3 @@
-use failure::Error;
-
 mod parse {
     use super::Packet;
     use crate::parsers::unsigned;
@@ -36,24 +34,48 @@ mod parse {
         tuple((terminated(packet, newline), terminated(packet, newline)))(input)
     }
 
-    fn pairs(input: &str) -> IResult<&str, Box<[(Packet, Packet)]>> {
-        map(separated_list1(newline, pair), Vec::into_boxed_slice)(input)
+    fn pairs(input: &str) -> IResult<&str, Vec<(Packet, Packet)>> {
+        separated_list1(newline, pair)(input)
     }
 
-    pub fn parse_input(input: &str) -> Result<Box<[(Packet, Packet)]>, Error> {
+    pub fn parse_input(input: &str) -> Result<Vec<(Packet, Packet)>, Error> {
         all_consuming(pairs)(input)
             .map(|(_, pairs)| pairs)
             .map_err(|err| err_msg(format!("Failed to parse packets: {}", err)))
     }
 }
+use failure::{err_msg, Error};
 
+use itertools::Itertools;
 use parse::parse_input;
-use std::cmp::Ordering;
+use std::{
+    cmp::Ordering,
+    fmt::{self, Display},
+};
 
-#[derive(Eq, PartialEq, Debug)]
+#[derive(Eq, PartialEq, Debug, Clone)]
 pub enum Packet {
     List(Box<[Packet]>),
     Number(u64),
+}
+
+impl Display for Packet {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Packet::*;
+        match self {
+            Number(x) => write!(f, "{}", x),
+            List(xs) => {
+                write!(f, "[")?;
+                for (index, x) in xs.iter().enumerate() {
+                    if index > 0 {
+                        write!(f, ",")?;
+                    }
+                    write!(f, "{}", x)?;
+                }
+                write!(f, "]")
+            }
+        }
+    }
 }
 
 impl PartialOrd for Packet {
@@ -84,10 +106,39 @@ fn indices_of_ordered_pairs(pairs: &[(Packet, Packet)]) -> impl Iterator<Item = 
         .filter_map(|(index, (x, y))| if x < y { Some(index) } else { None })
 }
 
+fn build_divider(num: u64) -> Packet {
+    Packet::List(
+        vec![Packet::List(vec![Packet::Number(num)].into_boxed_slice())].into_boxed_slice(),
+    )
+}
+
+fn find_packet(packets: &[Packet], packet: Packet) -> Option<usize> {
+    packets
+        .iter()
+        .find_position(|&p| *p == packet)
+        .map(|(index, _)| index + 1)
+}
+
+fn get_decoder_key(pairs: Vec<(Packet, Packet)>) -> Result<usize, Error> {
+    let divider_one = build_divider(2);
+    let divider_two = build_divider(6);
+
+    let mut all_packets: Vec<Packet> = pairs.into_iter().flat_map(|(x, y)| [x, y]).collect();
+    all_packets.extend([divider_one.clone(), divider_two.clone()]);
+    all_packets.sort();
+
+    let position_one = find_packet(&all_packets, divider_one)
+        .ok_or_else(|| err_msg("Failed to find first divider"))?;
+    let position_two = find_packet(&all_packets, divider_two)
+        .ok_or_else(|| err_msg("Failed to find second divider"))?;
+
+    Ok(position_one * position_two)
+}
+
 pub struct Solver {}
 
 impl super::Solver for Solver {
-    type Problem = Box<[(Packet, Packet)]>;
+    type Problem = Vec<(Packet, Packet)>;
 
     fn parse_input(data: String) -> Result<Self::Problem, Error> {
         parse_input(&data)
@@ -95,6 +146,9 @@ impl super::Solver for Solver {
 
     fn solve(pairs: Self::Problem) -> (Option<String>, Option<String>) {
         let part_one = indices_of_ordered_pairs(&pairs).sum::<usize>().to_string();
-        (Some(part_one), None)
+        let part_two = get_decoder_key(pairs)
+            .expect("Failed to solve part two")
+            .to_string();
+        (Some(part_one), Some(part_two))
     }
 }
