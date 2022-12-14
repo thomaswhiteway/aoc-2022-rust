a09a61438f8670ba84213f318d974cec62ca36bc
Thomas Whiteway <thomas.whiteway@gmail.com>
2022-12-14 09:33:12 +0000

    Day 14 Part 2

--- src/day14.rs
+++ src/day14.rs
@@ -45,14 +45,16 @@ impl Path {
 struct Contents {
     contents: HashMap<Position, Filler>,
     max_y: i64,
+    floor: Option<i64>,
 }
 
 impl Contents {
-    fn new(rocks: HashMap<Position, Filler>) -> Self {
+    fn new(rocks: HashMap<Position, Filler>, floor_offset: Option<i64>) -> Self {
         let max_y = rocks.keys().map(|p| p.y).max().unwrap();
         Contents {
             contents: rocks,
             max_y,
+            floor: floor_offset.map(|delta| max_y + delta),
         }
     }
 
@@ -61,11 +63,11 @@ impl Contents {
     }
 
     fn is_out_of_bounds(&self, position: Position) -> bool {
-        position.y > self.max_y
+        self.floor.is_none() && position.y > self.max_y
     }
 
     fn is_occupied(&self, position: Position) -> bool {
-        self.contents.contains_key(&position)
+        self.contents.contains_key(&position) || Some(position.y) == self.floor
     }
 }
 
@@ -75,13 +77,13 @@ enum Filler {
     Sand,
 }
 
-fn draw_paths(paths: &[Path]) -> Contents {
+fn draw_paths(paths: &[Path], floor_offset: Option<i64>) -> Contents {
     let rocks = paths
         .iter()
         .flat_map(|path| path.positions())
         .map(|point| (point, Filler::Rock))
         .collect();
-    Contents::new(rocks)
+    Contents::new(rocks, floor_offset)
 }
 
 fn next_step(contents: &Contents, position: Position) -> Option<Position> {
@@ -91,8 +93,8 @@ fn next_step(contents: &Contents, position: Position) -> Option<Position> {
         .find(|pos| !contents.is_occupied(*pos))
 }
 
-fn drop_grain(contents: &mut Contents) -> Option<Position> {
-    let mut position = Position { x: 500, y: 0 };
+fn drop_grain(contents: &mut Contents, start_position: Position) -> Option<Position> {
+    let mut position = start_position;
 
     while let Some(next_position) = next_step(contents, position) {
         position = next_position;
@@ -106,8 +108,12 @@ fn drop_grain(contents: &mut Contents) -> Option<Position> {
 }
 
 fn fill_sand(contents: &mut Contents) -> usize {
+    let start_position = Position { x: 500, y: 0 };
     for index in 0.. {
-        if let Some(position) = drop_grain(contents) {
+        if contents.is_occupied(start_position) {
+            return index;
+        }
+        if let Some(position) = drop_grain(contents, start_position) {
             contents.add_grain(position);
         } else {
             return index;
@@ -116,8 +122,8 @@ fn fill_sand(contents: &mut Contents) -> usize {
     0
 }
 
-fn num_grains_to_stick(paths: &[Path]) -> usize {
-    let mut contents = draw_paths(paths);
+fn num_grains_to_stick(paths: &[Path], floor_offset: Option<i64>) -> usize {
+    let mut contents = draw_paths(paths, floor_offset);
     fill_sand(&mut contents)
 }
 
@@ -131,7 +137,8 @@ impl super::Solver for Solver {
     }
 
     fn solve(paths: Self::Problem) -> (Option<String>, Option<String>) {
-        let part_one = num_grains_to_stick(&paths).to_string();
-        (Some(part_one), None)
+        let part_one = num_grains_to_stick(&paths, None).to_string();
+        let part_two = num_grains_to_stick(&paths, Some(2)).to_string();
+        (Some(part_one), Some(part_two))
     }
 }
