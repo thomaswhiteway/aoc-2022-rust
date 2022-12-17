0ef9d8571238fb88e96ef8cbafb587b291271571
Thomas Whiteway <thomas.whiteway@gmail.com>
2022-12-17 22:17:03 +0000

    Tidy day 17

--- src/day17.rs
+++ src/day17.rs
@@ -1,6 +1,11 @@
 use failure::{err_msg, Error};
-use std::collections::HashMap;
-use std::{cmp::max, collections::HashSet, str::FromStr};
+use std::{
+    cmp::{max, min},
+    collections::HashMap,
+    collections::HashSet,
+    ops::Range,
+    str::FromStr,
+};
 
 use crate::common::Position;
 
@@ -36,6 +41,7 @@ impl TryFrom<char> for Direction {
 struct Rock {
     offsets: Vec<Position>,
     width: i64,
+    height: i64,
 }
 
 impl Rock {
@@ -66,53 +72,33 @@ impl FromStr for Rock {
             })
             .collect();
         let width = offsets.iter().map(|pos| pos.x).max().unwrap() + 1;
-        Ok(Rock { offsets, width })
+        let height = offsets.iter().map(|pos| pos.y).max().unwrap() + 1;
+
+        Ok(Rock {
+            offsets,
+            width,
+            height,
+        })
     }
+}
+
+#[derive(PartialEq, Eq, Debug, Clone, Copy)]
+enum Collision {
+    Wall,
+    Floor,
+    Rocks(usize),
 }
 
 struct Tower {
-    filled: HashSet<Position>,
+    filled: HashMap<Position, usize>,
     max_y: i64,
     width: i64,
-}
-
-#[allow(unused)]
-fn draw(tower: &Tower, rock: Option<(&Rock, Position)>, rows: usize) {
-    let rock_positions = if let Some((rock, position)) = rock {
-        rock.positions_at(position).collect::<HashSet<_>>()
-    } else {
-        HashSet::new()
-    };
-
-    for y in (0..=tower.max_y + 4).rev().take(rows) {
-        print!("|");
-        for x in 0..tower.width {
-            let position = (x, y).into();
-            if rock_positions.contains(&position) {
-                print!("@");
-            } else if tower.filled.contains(&position) {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        }
-        print!("|\n");
-    }
-    if tower.height() as usize > rows {
-        println!("...\n\n");
-    } else {
-        print!("+");
-        for _ in 0..tower.width {
-            print!("-");
-        }
-        print!("+\n\n")
-    }
 }
 
 impl Tower {
     fn new(width: i64) -> Self {
         Tower {
-            filled: HashSet::new(),
+            filled: HashMap::new(),
             max_y: -1,
             width,
         }
@@ -122,26 +108,63 @@ impl Tower {
         self.max_y + 1
     }
 
-    fn add_rock(&mut self, positions: &[Position]) {
-        assert!(positions.iter().all(|pos| pos.x >= 0 && pos.x < self.width));
+    fn add_rock(&mut self, rock: &Rock, position: Position, index: usize) {
+        let positions = rock.positions_at(position).collect::<Vec<_>>();
         self.max_y = max(self.max_y, positions.iter().map(|pos| pos.y).max().unwrap());
-        self.filled.extend(positions);
+        self.filled
+            .extend(positions.into_iter().map(|position| (position, index)));
+    }
+
+    fn check_collision(&self, rock: &Rock, position: Position) -> Option<Collision> {
+        if position.y < 0 {
+            Some(Collision::Floor)
+        } else if position.x < 0 || position.x + rock.width > self.width {
+            Some(Collision::Wall)
+        } else {
+            let rocks = rock
+                .positions_at(position)
+                .filter_map(|pos| self.filled.get(&pos));
+
+            rocks.max().map(|latest| Collision::Rocks(*latest))
+        }
     }
 
     fn can_fit(&self, rock: &Rock, position: Position) -> bool {
-        position.y >= 0
-            && position.x >= 0
-            && position.x + rock.width <= self.width
-            && self
-                .filled
-                .is_disjoint(&rock.positions_at(position).collect::<HashSet<_>>())
+        self.check_collision(rock, position).is_none()
     }
-}
 
-struct SimulationResult {
-    num_rocks: usize,
-    num_steps: usize,
-    tower: Tower,
+    #[allow(unused)]
+    fn draw(&self, rock: Option<(&Rock, Position)>, rows: usize) {
+        let rock_positions = if let Some((rock, position)) = rock {
+            rock.positions_at(position).collect::<HashSet<_>>()
+        } else {
+            HashSet::new()
+        };
+
+        for y in (0..=self.max_y + 4).rev().take(rows) {
+            print!("|");
+            for x in 0..self.width {
+                let position = (x, y).into();
+                if rock_positions.contains(&position) {
+                    print!("@");
+                } else if self.filled.contains_key(&position) {
+                    print!("#");
+                } else {
+                    print!(".");
+                }
+            }
+            println!("|");
+        }
+        if self.height() as usize > rows {
+            println!("...\n\n");
+        } else {
+            print!("+");
+            for _ in 0..self.width {
+                print!("-");
+            }
+            print!("+\n\n")
+        }
+    }
 }
 
 fn get_rocks() -> Box<[Rock]> {
@@ -158,6 +181,28 @@ fn get_rocks() -> Box<[Rock]> {
     .into_boxed_slice()
 }
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum Action<T> {
+    Stop(T),
+    Continue,
+}
+
+struct FallenRock<'a> {
+    position: Position,
+    rock: &'a Rock,
+    collision: Collision,
+}
+
+trait Watcher {
+    type Output;
+    fn watch(
+        &mut self,
+        old_state: &State,
+        new_state: &State,
+        fallen_rock: &FallenRock,
+    ) -> Action<Self::Output>;
+}
+
 fn move_sideways(position: &mut Position, rock: &Rock, direction: Direction, tower: &Tower) {
     let next_position = *position + direction.offset();
 
@@ -166,75 +211,88 @@ fn move_sideways(position: &mut Position, rock: &Rock, direction: Direction, tow
     }
 }
 
-fn move_down(position: &mut Position, rock: &Rock, tower: &Tower) -> bool {
+fn move_down(position: &mut Position, rock: &Rock, tower: &Tower) -> Option<Collision> {
     let next_position = Position {
         x: position.x,
         y: position.y - 1,
     };
-    if tower.can_fit(rock, next_position) {
-        *position = next_position;
-        true
+    if let Some(collision) = tower.check_collision(rock, next_position) {
+        Some(collision)
     } else {
-        false
+        *position = next_position;
+        None
     }
 }
 
-fn drop_rock(
-    rock: &Rock,
+fn drop_rock<'a>(
+    rock: &'a Rock,
     jets: &mut impl Iterator<Item = Direction>,
     tower: &Tower,
     from: Position,
-) -> (usize, Position) {
+) -> (usize, FallenRock<'a>) {
     let mut position = from;
 
-    for (step, jet) in jets.enumerate() {
+    for (index, jet) in jets.enumerate() {
         move_sideways(&mut position, rock, jet, tower);
-        if !move_down(&mut position, rock, tower) {
-            return (step + 1, position);
+        if let Some(collision) = move_down(&mut position, rock, tower) {
+            return (
+                index + 1,
+                FallenRock {
+                    position,
+                    rock,
+                    collision,
+                },
+            );
         }
     }
 
     panic!("Ran out of jets")
 }
 
-fn simulate_dropping_rocks<'a, F, G>(
+#[derive(Default, Clone)]
+struct State {
+    num_rocks: usize,
+    num_steps: usize,
+    height: i64,
+}
+
+impl State {
+    fn update(&self, num_steps: usize, tower: &Tower) -> Self {
+        State {
+            num_rocks: self.num_rocks + 1,
+            num_steps: self.num_steps + num_steps,
+            height: tower.height(),
+        }
+    }
+}
+
+fn drop_rocks<'a, W: Watcher>(
     rocks: impl Iterator<Item = &'a Rock>,
     mut jets: impl Iterator<Item = Direction>,
-    mut stop: F,
-    display: G,
-) -> SimulationResult
-where
-    F: FnMut(usize, usize, &Tower, Position) -> bool,
-    G: Fn(usize) -> bool
-{
+    mut watcher: W,
+    display: Draw,
+) -> W::Output {
     let mut tower = Tower::new(TOWER_WIDTH);
-    let mut total_steps = 0;
+    let mut state = State::default();
+
     for (dropped_rocks, rock) in rocks.enumerate() {
-        let (steps, end_position) = drop_rock(
-            rock,
-            &mut jets,
-            &tower,
-            Position {
-                x: 2,
-                y: tower.max_y + 4,
-            },
-        );
-        total_steps += steps;
+        let drop_position = Position {
+            x: 2,
+            y: tower.max_y + 4,
+        };
+        let (num_steps, fallen_rock) = drop_rock(rock, &mut jets, &tower, drop_position);
 
-        if display(dropped_rocks + 1) {
-            println!("After {} rocks:", dropped_rocks + 1);
-            draw(&tower, Some((rock, end_position)), 20);
+        display.draw_tower(dropped_rocks + 1, &tower, &fallen_rock);
+
+        tower.add_rock(rock, fallen_rock.position, dropped_rocks + 1);
+
+        let new_state = state.update(num_steps, &tower);
+
+        if let Action::Stop(outcome) = watcher.watch(&state, &new_state, &fallen_rock) {
+            return outcome;
         }
 
-        tower.add_rock(&rock.positions_at(end_position).collect::<Vec<_>>());
-        //println!("{},{},{},{},{}", dropped_rocks + 1, total_steps, tower.height(), end_position.x, end_position.y);
-        if stop(dropped_rocks + 1, total_steps, &tower, end_position) {
-            return SimulationResult {
-                num_rocks: dropped_rocks + 1,
-                num_steps: total_steps,
-                tower,
-            };
-        }
+        state = new_state;
     }
     panic!("Ran out of rocks");
 }
@@ -243,7 +301,7 @@ where
 struct CycleIndex {
     rock_index: usize,
     jet_index: usize,
-    x: i64
+    x: i64,
 }
 
 impl CycleIndex {
@@ -251,128 +309,173 @@ impl CycleIndex {
         CycleIndex {
             rock_index,
             jet_index,
-            x
+            x,
         }
     }
 }
 
-struct CycleState {
-    num_dropped_rocks: usize,
-    num_steps: usize,
-    tower_height: i64,
-    last_rock_position: Position,
+enum Draw {
+    Never,
+    #[allow(unused)]
+    Ranges(Vec<Range<usize>>),
+}
+
+impl Draw {
+    fn draw_tower(&self, dropped_rocks: usize, tower: &Tower, state: &FallenRock<'_>) {
+        if let Draw::Ranges(ranges) = self {
+            if ranges.iter().any(|range| range.contains(&dropped_rocks)) {
+                println!("After {} rocks:", dropped_rocks + 1);
+                tower.draw(Some((state.rock, state.position)), 20);
+            }
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 struct Segment {
-    num_rocks: usize,
-    num_steps: usize,
-    height_delta: i64,
+    height_deltas: Vec<i64>,
 }
 
-impl From<SimulationResult> for Segment {
-    fn from(res: SimulationResult) -> Self {
+impl Segment {
+    fn total_height(&self) -> i64 {
+        *self.height_deltas.last().unwrap()
+    }
+
+    fn height_after_rocks(&self, num_rocks: usize) -> i64 {
+        if num_rocks > 0 {
+            self.height_deltas[num_rocks - 1]
+        } else {
+            0
+        }
+    }
+
+    fn cycle_height_after_rocks(&self, num_rocks: usize) -> i64 {
+        (num_rocks / self.num_rocks()) as i64 * self.total_height()
+            + self.height_after_rocks(num_rocks % self.num_rocks())
+    }
+
+    fn num_rocks(&self) -> usize {
+        self.height_deltas.len()
+    }
+}
+
+struct GetHeightAfter {
+    num_rocks: usize,
+}
+
+impl GetHeightAfter {
+    #[allow(unused)]
+    fn new(num_rocks: usize) -> Self {
+        GetHeightAfter { num_rocks }
+    }
+}
+
+impl Watcher for GetHeightAfter {
+    type Output = i64;
+    fn watch(
+        &mut self,
+        _old_state: &State,
+        new_state: &State,
+        _fallen_rock: &FallenRock,
+    ) -> Action<Self::Output> {
+        if new_state.num_rocks == self.num_rocks {
+            Action::Stop(new_state.height)
+        } else {
+            Action::Continue
+        }
+    }
+}
+
+struct CycleFinder {
+    rock_cycle_len: usize,
+    jet_cycle_len: usize,
+    visited: HashMap<CycleIndex, usize>,
+    heights: Vec<i64>,
+}
+
+impl CycleFinder {
+    fn new(rock_cycle_len: usize, jet_cycle_len: usize) -> Self {
+        CycleFinder {
+            rock_cycle_len,
+            jet_cycle_len,
+            visited: HashMap::default(),
+            heights: Vec::new(),
+        }
+    }
+
+    fn cycle_index(&self, state: &State, fallen_rock: &FallenRock) -> CycleIndex {
+        CycleIndex::new(
+            state.num_rocks % self.rock_cycle_len,
+            state.num_steps % self.jet_cycle_len,
+            fallen_rock.position.x,
+        )
+    }
+
+    fn segment(&self, range: Range<usize>) -> Segment {
+        let initial_height = if range.start == 0 {
+            0
+        } else {
+            self.heights[range.start - 1]
+        };
         Segment {
-            num_rocks: res.num_rocks,
-            num_steps: res.num_steps,
-            height_delta: res.tower.height(),
+            height_deltas: self.heights[range]
+                .iter()
+                .map(|height| height - initial_height)
+                .collect(),
+        }
+    }
+}
+
+impl Watcher for CycleFinder {
+    type Output = (Segment, Segment);
+    fn watch(
+        &mut self,
+        old_state: &State,
+        new_state: &State,
+        fallen_rock: &FallenRock,
+    ) -> Action<Self::Output> {
+        self.heights.push(new_state.height);
+
+        if let Collision::Rocks(latest) = fallen_rock.collision {
+            self.visited.retain(|_, num_rocks| *num_rocks <= latest);
+        } else if fallen_rock.collision == Collision::Floor {
+            self.visited.clear();
+        }
+
+        // Only consider starting a cycle where a rock has fallen in a way where
+        // there's a clean break between that rock and any previous rocks.
+        if new_state.height - old_state.height == fallen_rock.rock.height {
+            let cycle_index = self.cycle_index(new_state, fallen_rock);
+
+            if let Some(prefix_len) = self.visited.insert(cycle_index, new_state.num_rocks) {
+                let prefix = self.segment(0..prefix_len);
+                let cycle = self.segment(prefix_len..new_state.num_rocks);
+                Action::Stop((prefix, cycle))
+            } else {
+                Action::Continue
+            }
+        } else {
+            Action::Continue
         }
     }
 }
 
 fn find_prefix_and_cycle_time(jets: &[Direction], rocks: &[Rock]) -> (Segment, Segment) {
-    let mut visited: HashMap<CycleIndex, CycleState> = HashMap::new();
-    let mut prefix = None;
-
-    let can_start_cycle =
-        |num_dropped_rocks: usize, last_rock_position: Position, tower: &Tower| {
-            num_dropped_rocks % rocks.len() == 1
-                && last_rock_position.x > 0
-                && last_rock_position.x < 3
-                && (0..tower.width)
-                    .filter(|x| {
-                        tower.filled.contains(&Position {
-                            x: *x,
-                            y: last_rock_position.y,
-                        })
-                    })
-                    .count()
-                    == 4
-        };
-
-    let outcome = simulate_dropping_rocks(
+    drop_rocks(
         rocks.iter().cycle(),
         jets.iter().cloned().cycle(),
-        |num_dropped_rocks, num_steps, tower, last_rock_position| {
-            visited.retain(|_, state| state.last_rock_position.y < last_rock_position.y);
-            if can_start_cycle(num_dropped_rocks, last_rock_position, tower) {
-                let key = CycleIndex::new(num_dropped_rocks % rocks.len(), num_steps % jets.len(), last_rock_position.x);
-                let state = CycleState {
-                    num_dropped_rocks,
-                    num_steps,
-                    tower_height: tower.height(),
-                    last_rock_position,
-                };
-                if let Some(prefix_state) = visited.insert(key, state) {
-                    prefix = Some(Segment {
-                        num_rocks: prefix_state.num_dropped_rocks,
-                        num_steps: prefix_state.num_steps,
-                        height_delta: prefix_state.tower_height,
-                    });
-                    true
-                } else {
-                    false
-                }
-            } else {
-                false
-            }
-        },
-        |_| false
-    );
-
-    let prefix = prefix.unwrap();
-    let cycle = Segment {
-        num_rocks: outcome.num_rocks - prefix.num_rocks,
-        num_steps: outcome.num_steps - prefix.num_steps,
-        height_delta: outcome.tower.height() - prefix.height_delta,
-    };
-    (prefix, cycle)
+        CycleFinder::new(rocks.len(), jets.len()),
+        Draw::Never,
+    )
 }
 
-fn find_height_after_by_simulating<'a>(
-    rocks: impl Iterator<Item = &'a Rock>,
-    jets: impl Iterator<Item = Direction>,
-    num_rocks: usize,
-) -> i64 {
-    simulate_dropping_rocks(rocks, jets, |dropped_rocks, _, _, _| {
-        dropped_rocks == num_rocks
-    }, |_| false)
-    .tower
-    .height()
-}
+fn find_height_after(rocks: &[Rock], jets: &[Direction], num_rocks: usize) -> i64 {
+    let (prefix, cycle) = find_prefix_and_cycle_time(jets, rocks);
 
-fn find_height_after(rocks: &[Rock], jets: &[Direction], mut num_rocks: usize) -> i64 {
-    let (prefix, cycle) = find_prefix_and_cycle_time(jets, &rocks);
+    let prefix_rocks = min(prefix.num_rocks(), num_rocks);
+    let cycle_rocks = num_rocks - prefix_rocks;
 
-    let mut total_height = 0;
-    if num_rocks > prefix.num_rocks {
-        total_height += prefix.height_delta;
-        num_rocks -= prefix.num_rocks;
-
-        total_height += (num_rocks / cycle.num_rocks) as i64 * cycle.height_delta;
-        num_rocks %= cycle.num_rocks;
-    }
-
-    total_height += find_height_after_by_simulating(
-        rocks.iter().cycle().skip(1),
-        jets.iter()
-            .cloned()
-            .cycle()
-            .skip(prefix.num_steps % jets.len()),
-        num_rocks,
-    );
-
-    total_height
+    prefix.height_after_rocks(prefix_rocks) + cycle.cycle_height_after_rocks(cycle_rocks)
 }
 
 pub struct Solver {}
@@ -390,6 +493,7 @@ impl super::Solver for Solver {
 
     fn solve(jets: Self::Problem) -> (Option<String>, Option<String>) {
         let rocks = get_rocks();
+
         let part_one = find_height_after(&rocks, &jets, 2022).to_string();
         let part_two = find_height_after(&rocks, &jets, 1000000000000).to_string();
         (Some(part_one), Some(part_two))
