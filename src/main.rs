397498fb51b88914004779d5325ff0742098c9c1
Thomas Whiteway <thomas.whiteway@gmail.com>
2022-12-18 11:21:54 +0000

    Add ability to run all days

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,62 @@
 use aocf::Aoc;
-use failure::Error;
+use failure::{err_msg, Error};
 
-use std::path::PathBuf;
+use std::{path::PathBuf, time::Instant};
 use structopt::StructOpt;
 
 use aoc2022::{read_input, solve_day, Part};
 
 #[derive(StructOpt, Debug)]
 struct Opt {
-    day: u32,
+    day: Option<u32>,
     input: Option<PathBuf>,
 
     #[structopt(long)]
     submit: Option<Part>,
 }
 
-fn main() -> Result<(), Error> {
-    let opt = Opt::from_args();
-
+fn run_day(day: u32, input: Option<PathBuf>, submit: Option<Part>) -> Result<(), Error> {
     let mut aoc = Aoc::new()
         .parse_cli(false)
         .year(Some(2022))
-        .day(Some(opt.day))
+        .day(Some(day))
         .init()?;
 
-    let data = read_input(opt.input, &mut aoc)
+    let data = read_input(input, &mut aoc)
         .map_err(|err| failure::err_msg(format!("Failed to read input: {}", err)))?;
 
-    solve_day(opt.day, data, &mut aoc, opt.submit)?;
+    solve_day(day, data, &mut aoc, submit)?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), Error> {
+    let opt = Opt::from_args();
+
+    if let Some(day) = opt.day {
+        run_day(day, opt.input, opt.submit)?;
+    } else {
+        if opt.input.is_some() {
+            return Err(err_msg("Can't provide input for all days"));
+        }
+        if opt.submit.is_some() {
+            return Err(err_msg("Can't submit solution for all days"));
+        }
+        for day in 1..=25 {
+            println!("Day {}", day);
+            let start = Instant::now();
+            run_day(day, None, None)?;
+            let elapsed = start.elapsed();
+            if elapsed.as_secs() > 0 {
+                println!("Took {}.{:03}s", elapsed.as_secs(), elapsed.subsec_millis());
+            } else if elapsed.as_millis() > 0 {
+                println!("Took {}ms", elapsed.as_millis());
+            } else {
+                println!("Took {}µs", elapsed.as_micros());
+            }
+            println!();
+        }
+    }
 
     Ok(())
 }
